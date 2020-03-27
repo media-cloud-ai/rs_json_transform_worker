d130ff360b568473a841673440e0d32b42a7fe75
Mathia Haure-Touze <mathia.haure-touzet.ext@francetv.fr>
2020-03-27 09:23:40 +0100

    cargo fmt result

--- src/message.rs
+++ src/message.rs
@@ -4,22 +4,21 @@ use amqp_worker::ParametersContainer;
 use lapin_futures::Channel;
 
 pub fn process(
-  _channel: Option<&Channel>,
-  job: &Job,
-  job_result: JobResult,
+    _channel: Option<&Channel>,
+    job: &Job,
+    job_result: JobResult,
 ) -> Result<JobResult, MessageError> {
-  match job
-    .get_string_parameter("action")
-    .unwrap_or_else(|| "Undefined".to_string())
-    .as_str()
-  {
-    _ => hello_world(&job, job_result)
-  }
+    match job
+        .get_string_parameter("action")
+        .unwrap_or_else(|| "Undefined".to_string())
+        .as_str()
+    {
+        _ => hello_world(&job, job_result),
+    }
 }
 
 fn hello_world(_job: &Job, job_result: JobResult) -> Result<JobResult, MessageError> {
-
     debug!("Hello world!");
 
     Ok(job_result.with_status(JobStatus::Completed))
-  }
+}
